4aa5b98b93d6dca8a3134e982cddd3fc532b97c5
UmrzoqScope3 <utoshkentov@scope3.com>
2022-11-14 16:02:27 +0500

    feat: added Navbar component

---
+++ components/Navbar/NavItem.tsx
@@ -0,0 +1,13 @@
+
+
+interface NavItemProps {
+    name: string;
+}
+
+export const NavItem = ({ name }: NavItemProps) => {
+    return (
+        <li>
+            <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">{name}</a>
+        </li>
+    )
+}
---
+++ components/Navbar/index.ts
@@ -0,0 +1,1 @@
+export { Navbar as deafult } from './Navbar'
